a06fbb6e39af465281d97fdaa51899d83803f058
dvvzdc12-prog <q66578859@gmail.com>
2025-08-22 11:23:18 +0600

    ваш комментарий

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-const user = {
-  name: "Мухаммадзие",
-  age: 14
-};
+let users = [
+  {id: 1, name: "Вася"},
+  {id: 2, name: "Петя"},
+  {id: 3, name: "Маша"}
+];
 
-const cacl = prompt("Введите name или age");
-if (cacl == "name") {
-  alert(user.name);
-}
-else if (cacl == "age") {
-  alert(user.age);
-}
+
+let arr = users.find(cacl => cacl.id === 2);
+console.log(arr); 
+
+
+let arr2 = users.filter(cacl => cacl.id > 1);
+console.log(arr2);
